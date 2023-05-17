4335dcee6ba9b67ec6e08c1509603c8e4e5de755
Ansh Agarwal <ansh.agarwal@BLRETV-FFN2AMD6M.local>
2023-05-17 17:39:10 +0530

    First Java commit

---
+++ src/com/flipkart/demo/Demo.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package com.flipkart.demo;
+
+import java.nio.file.FileSystemAlreadyExistsException;
+
+/**
+ * @author ansh.agarwal
+ *
+ */
+public class Demo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("My first program");
+	}
+
+}
